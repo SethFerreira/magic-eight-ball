356d67904ae99f1517c10052dbffde5216bc78d2
Seth Ferreira <semfware@gmail.com>
2025-07-25 14:40:04 -0400

    Refactor control flow

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 let randomNumber = Math.floor(Math.random() * 8);
-// Logs user name to the console
+// Input user name to the console
 const userName = 'Seth';
+// Ask a question (no question mark needed)
+const userQuestion = 'will I get a puppy';
+
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
-// The Magic Eight Ball asks a question to the user
-const userQuestion = 'The magic Eight Ball asks,';
-console.log(`${userQuestion} ${userName}`);
+console.log(`You ask, ${userQuestion}? The Magic Eight Ball says`);
 
 let eightBall = '';
+switch (eightBall = randomNumber) {
+  case randomNumber = 0:
+    randomNumber = 'It is certain';
+    break;
+  case randomNumber = 1:
+    randomNumber = 'It is decidedly so'; 
+    break;
+  case randomNumber = 2:
+    randomNumber = 'Reply hazy try again';
+    break;
+  case randomNumber = 3:
+    randomNumber = 'Cannot predict now';
+    break;
+  case randomNumber = 4:
+    randomNumber = 'Do not count on it';
+    break;
+  case randomNumber = 5:
+    randomNumber = 'My sources say no';
+    break;
+  case randomNumber = 6:
+    randomNumber = 'Outlook not so good';
+    break;
+  default: 
+    randomNumber = 'Signs point to yes';
+    break;
+}
+console.log(eightBall = randomNumber);
+
+/*
 if (randomNumber === 0) {
   randomNumber = 'It is certain';
 } else if (eightBall === 1) {
@@ -25,3 +55,4 @@ if (randomNumber === 0) {
   randomNumber = 'Signs point to yes';
 }
 console.log(eightBall = randomNumber);
+*/
