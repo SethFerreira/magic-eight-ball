73acf5a955c07035828bed70e3339dda425943bc
Seth Ferreira <semfware@gmail.com>
2025-07-28 18:44:58 -0400

    Add title

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let randomNumber = Math.floor(Math.random() * 8);
 // Input user name to the console
 const userName = 'Seth';
 // Ask a question (no question mark needed)
-const userQuestion = 'will I get a puppy';
+const userQuestion = 'Can I have a puppy';
 
 userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 console.log(`You ask, ${userQuestion}? The Magic Eight Ball says`);
@@ -35,24 +35,3 @@ switch (eightBall = randomNumber) {
     break;
 }
 console.log(eightBall = randomNumber);
-
-/*
-if (randomNumber === 0) {
-  randomNumber = 'It is certain';
-} else if (eightBall === 1) {
-  randomNumber = 'It is decidedly so';
-} else if (randomNumber === 2) {
-  randomNumber = 'Reply hazy try again';
-} else if (randomNumber === 3) {
-  randomNumber = 'Cannot predict now';
-} else if (randomNumber === 4) {
-  randomNumber = 'Do not count on it';
-} else if (randomNumber === 5) {
-  randomNumber = 'My sources say no';
-} else if (randomNumber === 6) {
-  randomNumber = 'Outlook not so good';
-} else {
-  randomNumber = 'Signs point to yes';
-}
-console.log(eightBall = randomNumber);
-*/
